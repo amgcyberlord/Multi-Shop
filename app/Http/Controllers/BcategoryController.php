6afe4934d0611cf970c8efdde4b4a229edb84415
amgcyberlord <charles.adu@yahoo.com>
2021-11-24 01:20:04 +0800

    Added Some Files

---
+++ app/Http/Controllers/BcategoryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Bcategory;
+use Illuminate\Http\Request;
+
+class BcategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Bcategory  $bcategory
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Bcategory $bcategory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Bcategory  $bcategory
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Bcategory $bcategory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Bcategory  $bcategory
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Bcategory $bcategory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Bcategory  $bcategory
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Bcategory $bcategory)
+    {
+        //
+    }
+}
